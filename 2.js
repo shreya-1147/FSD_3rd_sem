ff721104821f5e5d368f1a7023bb141e0d48bae3
shreya-1147 <shreya9236094193@gmail.com>
2026-09-11 14:49:20 +0530

    commit

---
+++ 2.js
@@ -0,0 +1,10 @@
+const EventEmitter=require('events');
+const app=new EventEmitter();
+app.on('login',(user)=>{
+    console.log('${user} logged in');
+});
+app.on('message',(msg)=>{
+    console.log('Message: ${msg}');
+});
+app.emit('login','Shreya');
+app.emit('message','Welcome to Node.js');
